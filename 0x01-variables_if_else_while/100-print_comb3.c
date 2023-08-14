94114b4dc4d9e6b45724a3a564b2f4c13eb86281
Uchendumbila <ekeuche1408@gmail.com>
2023-08-15 00:43:08 +0100

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Printing all possible different combinations of two digits
+ * numbers should be printed in ascending order
+ * numbers to be seperated by , followed by a space
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (n = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
