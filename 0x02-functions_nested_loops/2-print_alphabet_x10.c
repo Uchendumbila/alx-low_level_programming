57a22fd97771a19804603a3ccf6ca9efec629171
Uchendumbila <ekeuche1408@gmail.com>
2023-08-17 05:45:26 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - printing alphabet 10 times
+ * print_alphabet_x10 - printing alphabet 10 times
  * in lowercase, followed by a new line
  * Return: void
  */
