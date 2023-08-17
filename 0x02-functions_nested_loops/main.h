97c4b3dbfc674b07083fa3a391b940a10485b250
Uchendumbila <ekeuche1408@gmail.com>
2023-08-17 02:06:20 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+void jack_bauer(void);
+int print_last_digit(int);
+void times_table(void);
+void print_times_table(int n);
+void print_to_98(int n);
+int add(int, int);
