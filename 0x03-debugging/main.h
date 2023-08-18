e04c5706adf381ba4f1821eed2941d5bf8e99e26
Uchendumbila <ekeuche1408@gmail.com>
2023-08-18 13:46:24 -0700

    0-main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#define MAIN_H
+#ifndef MAIN_H
+#include <stdio.h>
+
+int largest_number(int a, int b, int c);
+int convert_day(int mont, int day);
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
+
