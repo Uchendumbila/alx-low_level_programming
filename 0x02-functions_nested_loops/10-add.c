3456535c96fadf0a5e517a91f654d9e3cf55e00c
Uchendumbila <ekeuche1408@gmail.com>
2023-08-18 02:52:50 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 
 /**
  * add - Adding two integers values
- * @n1 - The first number to be computed
- * @n2 - The second number to be computed
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
  * Return: Integer value
  */
 
