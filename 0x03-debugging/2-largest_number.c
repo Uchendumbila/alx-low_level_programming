835f13984b6b334501f51399faae2538238b5591
Uchendumbila <ekeuche1408@gmail.com>
2023-08-20 16:38:56 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,8 +5,9 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest always.
+ * Return: largest always
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
