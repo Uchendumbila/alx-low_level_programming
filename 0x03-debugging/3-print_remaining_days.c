45cb9584be7bcc9bb98d1e8e2efa6dafcbf89f2b
Uchendumbila <ekeuche1408@gmail.com>
2023-08-23 02:34:55 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,6 @@ void print_remaining_days(int month, int day, int year)
 	if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
 	{
 		if (month >= 2 && day != 60)
-			
 			day++;
 
 		printf("Day of the year: %d\n", day);
@@ -24,9 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		if (month == 2 && day == 60)
-		
 			printf("Invalid date: %2d/%02d/%4d\n", month, day - 31, year);
-		
 		else
 		{
 			printf("Day of the year: %d\n", day);
