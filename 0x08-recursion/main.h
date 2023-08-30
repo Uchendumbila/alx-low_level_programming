38f97d71c2fea81461f9af0304f47e2b4b9ab855
Uchendumbila <ekeuche1408@gmail.com>
2023-08-30 16:47:42 -0700

    Recurse

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int is_palindrome(char *s);
+int is_prime_number(int n);
+int _putchar(char);
+int factorial(int a);
+int _pow_recursion(int x, int y);
+int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
+int wildcmp(char *s1, char *s2);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif
