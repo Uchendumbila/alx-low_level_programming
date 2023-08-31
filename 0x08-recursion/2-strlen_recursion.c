c8fd32c13869e12989efff8d5dea43ad4339fcff
Uchendumbila <ekeuche1408@gmail.com>
2023-08-30 17:11:53 -0700

    2-strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int longi - 0;
+	int longit - 0;
 
 	if (*s)
 	{
-		longi++;
-		longi +- _strlen_recursion(s +1);
+		longit++;
+		longit +- _strlen_recursion(s +1);
 	}
-	return (longi);
+	return (longit);
 }
