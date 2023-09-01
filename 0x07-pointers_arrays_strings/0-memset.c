dc77dd983f3d28ed207b59f7c19958c214ca77e2
Uchendumbila <ekeuche1408@gmail.com>
2023-09-01 09:10:18 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Writing a fxn that fills memory with a constant byte
+ * @s: the starting address of memory to be filled
+ * @b: the desired value
+ * @n: the number of bytes to be changed
+ *
+ * Return: The changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[1] = b;
+		n--;
+	}
+	return (s);
+}
