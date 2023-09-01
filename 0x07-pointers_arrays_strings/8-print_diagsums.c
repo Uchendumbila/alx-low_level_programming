8e32a36d9d2482528c46417f1c2085b511b6efb2
Uchendumbila <ekeuche1408@gmail.com>
2023-09-01 10:25:27 -0700

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Writing a fxn that prints the sum of the two diagonals
+ * of a square matrix of integers.
+ * @a: Input
+ * @size: Input
+ *
+ * Return: Always 0.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sumx, sumy, z;
+
+	sumx = 0;
+	sumy = 0;
+
+	for (z = 0; z < size; z++)
+	{
+		sumx = sumx + a[z * size + z];
+	}
+	for (z = size - 1; z >= 0; z--)
+	{
+		sumy += a[y * size + (size - z - 1)];
+	}
+	printf("%d, %d\n", sumx, sumy);
+}
