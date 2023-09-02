8db2cb6bd9107538de487a9a7f80e6e8a414fc47
Uchendumbila <ekeuche1408@gmail.com>
2023-09-02 12:28:09 -0700

    infinite_add_p-update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -41,6 +41,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + i) != '\0')
 		i++;
+	while (*(n2 + j) != '\0')
+		j++;
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
