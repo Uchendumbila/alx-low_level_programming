623db6aada887dd083c0a5ac2d8e126e97a7a9b8
Uchendumbila <ekeuche1408@gmail.com>
2023-09-03 20:02:53 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,15 @@
 
 int main(int argc, char **argv)
 {
-	int mult = atoi(argv[1]) * atoi(argv[2]);
+	int mult;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	mult = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", mult);
 
