02193d885fbfa3fb72d82ae96b70c47d4f5ce324
Uchendumbila <ekeuche1408@gmail.com>
2023-09-05 12:35:10 -0700

    100-argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Writing a fxn that concatenates all the arguments of my program.
+ * @ac: Input
+ * @av: pointer array
+ *
+ * Return: Always 0.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, x, y = 0, l = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][x]; x++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][x]; x++)
+		{
+			str[y] = av[i][x];
+			y++;
+		}
+		if (str[y] == '\0')
+		{
+			str[y++] = '\n';
+		}
+	}
+	return (str);
+}
