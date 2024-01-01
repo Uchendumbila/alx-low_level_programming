671b2f7c89e8c09664dc4295ba40f06a3f249be1
Uchendumbila <ekeuche1408@gmail.com>
2024-01-01 01:45:41 -0800

    1-3

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int p;
+
+	hash = 5381;
+	while ((p = *str++))
+	{
+		hash = ((hash << 5) + hash) + p; /* hash * 33 + p */
+	}
+	return (hash);
+}
